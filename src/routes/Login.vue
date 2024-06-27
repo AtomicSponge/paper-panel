7ad6ee545cdbc766400155b96b1b20a432ec396d
Matthew Evans <mevans+git@wtfsystems.net>
2024-06-26 21:03:17 -0400

    sync

--- src/routes/Login.vue
+++ src/routes/Login.vue
@@ -3,15 +3,34 @@
 </script>
 
 <template>
-<div>
-  <label for="username">Username: </label>
-  <input id="username" type="text"/>
-</div>
-<div>
-  <label for="userpwd" type="userpwd">Password: </label>
-  <input id="userpwd" type="password"/>
-</div>
+<section>
+  <div class="row">
+    <div class="col">
+      <label for="username">Username: </label>
+    </div>
+    <div class="col">
+      <input id="username" type="text"/>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      <label for="userpwd" type="userpwd">Password: </label>
+    </div>
+    <div class="col">
+      <input id="userpwd" type="password"/>
+    </div>
+  </div>
+</section>
 </template>
 
 <style scoped>
+section {
+  display: table
+}
+div .row {
+  display: table-row;
+}
+div .col {
+  display: table-cell;
+}
 </style>
