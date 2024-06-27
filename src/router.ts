36e0174196c8609674bb093895be38e5de91828d
Matthew Evans <mevans+git@wtfsystems.net>
2024-06-27 08:59:03 -0400

    sync

--- src/router.ts
+++ src/router.ts
@@ -4,15 +4,39 @@ const Login = () => import('./routes/Login.vue')
 const ServerList = () => import('./routes/ServerList.vue')
 const WorldList = () => import('./routes/WorldList.vue')
 
+const isAuthenticated = false
+
 const routes = [
-  { path: '/', component: Login },
-  { path: '/servers', component: ServerList },
-  { path: '/worlds', component: WorldList }
+  {
+    path: '/',
+    component: ServerList
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/servers',
+    name: 'Servers',
+    component: ServerList
+  },
+  {
+    path: '/worlds',
+    name: 'Worlds',
+    component: WorldList
+  }
 ]
 
 const router = createRouter({
   history: createMemoryHistory(),
   routes
+})
+
+router.beforeEach(async (to, _from) => {
+  if(!isAuthenticated && to.name !== 'Login') {
+    return { name: 'Login' }
+  }
 })
 
 export default router
