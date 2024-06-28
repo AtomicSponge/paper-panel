8893604fb7e6fc3182269aabc6f5a14e9481e929
Matthew Evans <mevans+git@wtfsystems.net>
2024-06-28 09:29:42 -0400

    sync

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,11 @@
 import { createRouter, createMemoryHistory } from 'vue-router'
 
-import { authStore } from './stores/authStore'
+import { authStore } from '@/stores/authStore'
 
-const Login = () => import('./routes/Login.vue')
-const User = () => import ('./routes/User.vue')
-const ServerList = () => import('./routes/ServerList.vue')
-const WorldList = () => import('./routes/WorldList.vue')
+const Login = () => import('@/routes/Login.vue')
+const User = () => import ('@/routes/User.vue')
+const ServerList = () => import('@/routes/ServerList.vue')
+const WorldList = () => import('@/routes/WorldList.vue')
 
 const routes = [
   { path: '/', component: ServerList },
@@ -22,6 +22,7 @@ const router = createRouter({
 
 router.beforeEach(async (to, _from) => {
   const isAuthenticated = authStore()
+  //to.meta.requiresAuth
   if(!isAuthenticated.value && to.path !== '/login') {
     return { path: '/login' }
   }
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  resolve: {
+    alias: [
+      { find: "@", replacement: path.resolve(__dirname, "./src") }
+    ]
+  },
   plugins: [ vue() ]
 })
