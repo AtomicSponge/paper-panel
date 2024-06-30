71746c4d81b191fc2640aedb9a8fe024ebbf0ed5
Matthew Evans <mevans+git@wtfsystems.net>
2024-06-30 11:51:22 -0400

    sync

--- pages/index/+Page.vue
+++ pages/index/+Page.vue
@@ -5,14 +5,73 @@
 -->
 
 <script lang="ts" setup>
-import Counter from './Counter.vue'
+import { ref } from 'vue'
+
+const username = ref()
+const password = ref()
+
+const handleLogin = () => {
+  if (username.value === 'matt' && password.value === 'matt') {
+    //
+  } else {
+    username.value = ''
+    password.value = ''
+    alert('Incorrect username or password!')
+  }
+}
 </script>
 
 <template>
-  <h1>Welcome</h1>
-  This page is:
-  <ul>
-    <li>Rendered to HTML.</li>
-    <li>Interactive. <Counter /></li>
-  </ul>
+<span>
+  <header>
+    <h3>Paper Panel Login</h3>
+  </header>
+  <section>
+    <div class="row">
+      <div class="col">
+        <label for="username">Username: </label>
+      </div>
+      <div class="col">
+        <input id="username" v-model="username" type="text" @keydown.enter="handleLogin"/>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <label for="userpwd" type="userpwd">Password: </label>
+      </div>
+      <div class="col">
+        <input id="userpwd" v-model="password" type="password" @keydown.enter="handleLogin"/>
+      </div>
+    </div>
+  </section>
+  <button @click="handleLogin">Login</button>
+</span>
 </template>
+
+<style scoped>
+span {
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+  height: 84vh;
+}
+header {
+  text-align: center;
+}
+section {
+  display: table;
+  margin-left: auto;
+  margin-right: auto;
+}
+div .row {
+  display: table-row;
+}
+div .col {
+  display: table-cell;
+  padding: 4px;
+}
+button {
+  margin: 12px;
+}
+</style>
