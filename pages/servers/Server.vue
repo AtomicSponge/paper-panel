d41644440309f847ed7c6eb9d9a7f9e7343e0a95
Matthew Evans <mevans+git@wtfsystems.net>
2024-06-30 19:24:55 -0400

    sync

--- pages/servers/Server.vue
+++ pages/servers/Server.vue
@@ -14,12 +14,36 @@ defineProps<{
 
 <template>
   <section>
-    <div>{{ name }}</div>
-    <div>{{ address }}: {{ port }}</div>
+    <div class="row">
+      <div class="cell">
+        {{ name }}
+      </div>
+    </div>
+    <div class="row">
+      <div class="cell">
+        {{ address }}:{{ port }}
+      </div>
+    </div>
   </section>
 </template>
 
 <style scoped>
+section {
+  display: table;
+  margin: 1em;
+  padding: 1em;
+  border: 1px solid rgb(84, 90, 44);
+  border-radius: 32px;
+  background-color: rgb(84, 90, 44);
+}
+div .row {
+  display: table-row
+}
+div .cell {
+  display: table-cell;
+  text-align: start;
+  padding: 1em;
+}
 
 @media (prefers-color-scheme: light) {
   /** */
