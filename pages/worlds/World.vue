72659c26346a2fa3c5826f345a5b28af18f1033a
Matthew Evans <mevans+git@wtfsystems.net>
2024-06-30 19:46:59 -0400

    sync

--- pages/worlds/World.vue
+++ pages/worlds/World.vue
@@ -14,8 +14,27 @@
 </template>
 
 <style scoped>
+section {
+  display: table;
+  margin: 1em;
+  padding: 1em;
+  border: 3px solid rgb(85, 85, 85);
+  border-radius: 32px;
+  background-color: rgb(55, 55, 55);
+}
+div .row {
+  display: table-row
+}
+div .cell {
+  display: table-cell;
+  text-align: start;
+  padding: 1em;
+}
 
 @media (prefers-color-scheme: light) {
-  /** */
+  section {
+    border: 3px solid rgb(255, 239, 213);
+    background-color: rgb(253, 245, 230);
+  }
 }
 </style>
