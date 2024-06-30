bb75488acb2cf7f900903ab632e2da85f437a944
Matthew Evans <mevans+git@wtfsystems.net>
2024-06-30 18:32:46 -0400

    sync

--- renderer/Navigation.vue
+++ renderer/Navigation.vue
@@ -1,8 +1,13 @@
 <script lang="ts" setup>
+import { usePageContext } from './usePageContext'
+
+const pageContext = usePageContext()
+
+const { username } = pageContext.value
 </script>
 
 <template>
-  <a href="/user">matt</a> >
+  <a href="/user">{{ username }}</a> >
   <a href="/servers">Servers</a> >
   <a href="/worlds">Worlds</a> >
   <a href="/world/world">World name</a>
