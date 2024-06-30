356842877f426e2b4cf497b471bff009cf139517
Matthew Evans <mevans+git@wtfsystems.net>
2024-06-30 15:47:46 -0400

    sync

--- renderer/Navigation.vue
+++ renderer/Navigation.vue
@@ -1,8 +1,11 @@
 <script lang="ts" setup>
+import type { Data } from './+data'
+import { useData } from './useData'
+const data = useData<Data>()
 </script>
 
 <template>
-  <a href="/user">user</a> >
+  <a href="/user">{{ data.username }}</a> >
   <a href="/servers">Servers</a> >
   <a href="/worlds">Worlds</a> >
   <a href="/world/world">World name</a>
