05ecaf2d07d7dc9e411febeed9fc2bff9b287391
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-01 08:12:37 -0400

    sync

--- layouts/HeadDefault.vue
+++ layouts/HeadDefault.vue
@@ -1,11 +1,13 @@
 <!-- Default <head> (can be overridden by pages) -->
 
+<script setup lang="ts">
+  import './css/index.css'
+  import logoUrl from "../assets/logo.svg"
+</script>
+
 <template>
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <meta name="description" content="Demo showcasing Vike" />
+  <meta name="description" content="Paper Panel" />
+  <meta http-equiv="Content-Security-Policy" content="script-src 'self' 'unsafe-inline'; style-src 'unsafe-inline';" />
   <link rel="icon" :href="logoUrl" />
 </template>
-
-<script setup lang="ts">
-  import logoUrl from "../assets/logo.svg";
-</script>
