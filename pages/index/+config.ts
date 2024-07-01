c8f99a1a93179e9c2831e850430f790fca95dbc0
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-01 14:20:28 -0400

    sync

---
+++ pages/index/+config.ts
@@ -0,0 +1,7 @@
+import Page from './+Page.vue'
+import Layout from '@/layouts/LayoutIndex.vue'
+
+export default {
+  Page,
+  Layout
+}
---
+++ pages/servers/@id/+config.ts
@@ -0,0 +1,7 @@
+import Page from './+Page.vue'
+import Layout from '@/layouts/LayoutAuthed.vue'
+
+export default {
+  Page,
+  Layout
+}
