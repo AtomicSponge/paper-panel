e0b395546a2d0f9a856cf49d72111d484cffc60b
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-01 10:39:49 -0400

    sync

--- layouts/LayoutDefault.vue
+++ layouts/LayoutDefault.vue
@@ -5,73 +5,11 @@
 -->
 
 <script lang="ts" setup>
-import { usePageContext } from 'vike-vue/usePageContext'
-import { useAttrs } from 'vue'
-
-import Navigation from './Navigation.vue'
-import AdminLink from './AdminLink.vue'
-
-const pageContext = usePageContext()
-const { urlPathname } = pageContext
-
-const { href } = useAttrs()
-
-console.log(urlPathname)
-console.log(href)
-
-const appURL = 'https://github.com/AtomicSponge/paper-panel#readme'
-const version = 'v0.0.1'
 </script>
 
 <template>
-  <section>
-    <header>
-      <nav class="left">
-        <Navigation />
-      </nav>
-      <nav class="right">
-        <a :href="appURL">Paper Panel</a>
-        {{ version }}
-      </nav>
-    </header>
-
-    <main>
-      <slot></slot>
-    </main>
-
-    <footer>
-      <nav class="right">
-        <AdminLink />
-      </nav>
-    </footer>
-  </section>
+  <slot></slot>
 </template>
 
 <style scoped>
-section {
-  display: flex;
-  flex-flow: column;
-  align-items: stretch;
-  height: 100vh;
-  padding-left: 6px;
-  padding-right: 6px;
-}
-header {
-  padding-top: 6px;
-  padding-bottom: 6px;
-}
-main {
-  flex: auto;
-  overflow: auto;
-}
-footer {
-  padding-top: 6px;
-  padding-bottom: 6px;
-}
-.left {
-  float: left;
-}
-.right {
-  float: right;
-}
 </style>
---
+++ pages/index/+Layout.vue
@@ -0,0 +1,57 @@
+<!--
+  paper-panel
+  By:  Matthew Evans
+  See LICENSE.md
+-->
+
+<script lang="ts" setup>
+const appURL = 'https://github.com/AtomicSponge/paper-panel#readme'
+const version = 'v0.0.1'
+</script>
+
+<template>
+  <section>
+    <header>
+      <nav class="right">
+        <a :href="appURL">Paper Panel</a>
+        {{ version }}
+      </nav>
+    </header>
+
+    <main>
+      <slot></slot>
+    </main>
+
+    <footer>
+    </footer>
+  </section>
+</template>
+
+<style scoped>
+section {
+  display: flex;
+  flex-flow: column;
+  align-items: stretch;
+  height: 100vh;
+  padding-left: 6px;
+  padding-right: 6px;
+}
+header {
+  padding-top: 6px;
+  padding-bottom: 6px;
+}
+main {
+  flex: auto;
+  overflow: auto;
+}
+footer {
+  padding-top: 6px;
+  padding-bottom: 6px;
+}
+.left {
+  float: left;
+}
+.right {
+  float: right;
+}
+</style>
