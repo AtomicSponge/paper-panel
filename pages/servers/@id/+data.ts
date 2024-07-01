998d154098459d2bf9487cfe208668638da30f78
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-01 12:14:15 -0400

    sync

--- pages/servers/@id/+data.ts
+++ pages/servers/@id/+data.ts
@@ -10,31 +10,39 @@ export { data }
 export type Data = Awaited<ReturnType<typeof data>>
 
 const data = async (pageContext:any) => {
-  const username = pageContext.username
+  const userId = Number(pageContext.routeParams.id)
 
-  const servers = [
+  const data = [
     {
       id: 1,
+      users: [ 1, 2 ],
       name: 'some name',
       address: 'localhost',
       port: 2345
     },
     {
       id: 2,
+      users: [ 1 ],
       name: 'another',
       address: '10.0.0.100',
       port: 4532
     },
     {
       id: 3,
+      users: [ 2 ],
       name: 'server three',
       address: '10.10.42.200',
       port: 4532
     }
   ]
 
+  const servers = data.map(server => {
+    server.users.filter(user => user === userId)
+  })
+
+  console.log(servers)
+
   return {
-    username,
-    servers
+    data
   }
 }
