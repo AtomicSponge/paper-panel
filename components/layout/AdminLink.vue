dddea85a4ad67950d062f546c3326dc8b2d3dd2f
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-02 11:28:12 -0400

    sync

--- components/layout/AdminLink.vue
+++ components/layout/AdminLink.vue
@@ -1,11 +1,3 @@
-<script lang="ts" setup>
-/*import { usePageContext } from './usePageContext'
-
-const pageContext = usePageContext()
-
-const { usertype } = pageContext.value*/
-</script>
-
 <template>
   <div>
     <a href="/admin">Admin Panel</a>
