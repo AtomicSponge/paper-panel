4eb892065ddb2e729be52428ca959cb5422967d5
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-05 08:31:12 -0400

    sync

--- pages/admin/+guard.ts
+++ pages/admin/+guard.ts
@@ -11,7 +11,7 @@ import { render } from 'vike/abort'
  
 const guard:GuardAsync = async (pageContext):ReturnType<GuardAsync> => {
   const { user } = pageContext
-  if (user === undefined || !user.admin) {
+  if (user === undefined) {
     throw render('/')
   }
   if (!user.admin) {
--- pages/worlds/+guard.ts
+++ pages/worlds/+guard.ts
@@ -7,11 +7,11 @@
 export { guard }
  
 import type { GuardAsync } from 'vike/types'
-import { redirect } from 'vike/abort'
+import { render } from 'vike/abort'
  
 const guard:GuardAsync = async (pageContext):ReturnType<GuardAsync> => {
   const { user } = pageContext
   if (user === undefined) {
-    throw redirect('/')
+    throw render('/')
   }
 }
