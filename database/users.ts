258020083826a40138af6711e4a8359c967fcf97
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-06 07:30:54 -0400

    sync

--- database/users.ts
+++ database/users.ts
@@ -10,12 +10,23 @@ import { JSONFileSyncPreset } from 'lowdb/node'
 interface Users {
   id:number
   name:string
+  password:string
+  salt:string
   serveradmin:boolean
   admin:boolean
 }
 
 const lowDb:LowSync<{ users:Users[] }> = JSONFileSyncPreset<{ users:Users[] }>('db.json', {
-  users: [],
+  users: [
+    {
+      id: 0,
+      name: 'admin',
+      password: '7d48fc1ebc7df52c32759fd620b64fe48907ebca9b3d396d61b68843a82f8b1813fa1fe27bffa6a6f075c2eebb988b948a16f721945117bd857e6fd8faa20b74',
+      salt: 'changeme',
+      serveradmin: true,
+      admin: true
+    }
+  ],
 })
 
 export { lowDb }
