6a65e47b9659c1a88457cab0334dbea43aaf6c92
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-06 07:25:29 -0400

    sync

--- database/users.ts
+++ database/users.ts
@@ -3,3 +3,20 @@
  * By:  Matthew Evans
  * See LICENSE.md
  */
+
+import type { LowSync } from 'lowdb'
+import { JSONFileSyncPreset } from 'lowdb/node'
+
+interface Users {
+  id:number
+  name:string
+  serveradmin:boolean
+  admin:boolean
+}
+
+const lowDb:LowSync<{ users:Users[] }> = JSONFileSyncPreset<{ users:Users[] }>('db.json', {
+  users: [],
+})
+
+export { lowDb }
+export type { Users }
