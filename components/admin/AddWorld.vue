8279af9d1a61cb26429707d6259fa78fb54bb82d
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-06 23:41:30 -0400

    sync

--- components/admin/AddWorld.vue
+++ components/admin/AddWorld.vue
@@ -5,8 +5,28 @@
 -->
 
 <script lang="ts" setup>
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+import AddWorldForm from '@/components/admin/AddWorldForm.vue'
+
+const newWorlds:Ref<string[]> = ref([])
+
+const addNewWorld = () => {
+  newWorlds.value.push('test')
+}
 </script>
 
 <template>
-  <button>Add a world</button>
+  <div>
+    <button @click="addNewWorld()">Add a world</button>
+  </div>
+  <div v-for="world in newWorlds">
+    <AddWorldForm/>
+  </div>
 </template>
+
+<style scoped>
+div {
+  padding: 0.4em 0;
+}
+</style>
---
+++ components/admin/AddWorldForm.vue
@@ -0,0 +1,15 @@
+<!--
+  paper-panel
+  By:  Matthew Evans
+  See LICENSE.md
+-->
+
+<script lang="ts" setup>
+</script>
+
+<template>
+  Form for adding a new world
+</template>
+
+<style scoped>
+</style>
