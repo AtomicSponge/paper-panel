47a6503b9c4941153943499870035086d14e4c8c
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-07 12:01:04 -0400

    sync

--- layouts/HeadDefault.vue
+++ layouts/HeadDefault.vue
@@ -6,7 +6,7 @@
 
 <script setup lang="ts">
   import './css/index.css'
-  import logoUrl from "@/assets/paper-panel-logo-512-512.png"
+  import logoUrl from "@/assets/paper-panel-logo-32-32.png"
 </script>
 
 <template>
