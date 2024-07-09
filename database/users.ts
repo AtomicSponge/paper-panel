8981e847c95b302165de9cf4c35b50d263518d96
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-09 14:47:26 -0400

    sync

--- database/users.ts
+++ database/users.ts
@@ -7,6 +7,7 @@
 export interface Users {
   id:number
   name:string
+  login:string
   password:string
   salt:string
   serveradmin:boolean
@@ -17,12 +18,30 @@ export const users = {
   users: [
     {
       id: 0,
-      name: 'admin',
+      name: 'Administrator',
+      login: 'admin',
       password: '7d48fc1ebc7df52c32759fd620b64fe48907ebca9b3d396d61b68843a82f8b1813fa1fe27bffa6a6f075c2eebb988b948a16f721945117bd857e6fd8faa20b74',
       salt: 'changeme',
       serveradmin: true,
       admin: true
+    },
+    {
+      id: 1,
+      name: 'Matthew',
+      login: 'matt',
+      password: '7d48fc1ebc7df52c32759fd620b64fe48907ebca9b3d396d61b68843a82f8b1813fa1fe27bffa6a6f075c2eebb988b948a16f721945117bd857e6fd8faa20b74',
+      salt: 'changeme',
+      serveradmin: true,
+      admin: false
+    },
+    {
+      id: 2,
+      name: 'Test',
+      login: 'test',
+      password: '7d48fc1ebc7df52c32759fd620b64fe48907ebca9b3d396d61b68843a82f8b1813fa1fe27bffa6a6f075c2eebb988b948a16f721945117bd857e6fd8faa20b74',
+      salt: 'changeme',
+      serveradmin: false,
+      admin: false
     }
   ],
 }
-
