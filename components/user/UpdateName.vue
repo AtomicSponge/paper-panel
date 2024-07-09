1503712a15955fe263f7dacffc2041f1dbe55030
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-09 16:13:21 -0400

    sync

---
+++ components/user/UpdateName.vue
@@ -0,0 +1,39 @@
+<!--
+  paper-panel
+  By:  Matthew Evans
+  See LICENSE.md
+-->
+
+<script lang="ts" setup>
+const props = defineProps<{
+  /** User name */
+  name:string
+}>()
+
+const updateName = async ():Promise<void> => {
+  //
+}
+</script>
+
+<template>
+  <div class="subbox">
+    <label for="username"><h3>Name:</h3></label>
+    <input id="username" type="text" :defaultValue="props.name"/>
+    <button @click="updateName()">Update</button>
+  </div>
+</template>
+
+<style scoped>
+div {
+  display: flex;
+  flex-flow: row;
+  align-items: center;
+  padding: 2em;
+}
+label {
+  margin-right: 1em;
+}
+input {
+  margin-right: 2em;
+}
+</style>
--- pages/user/@id/+Page.vue
+++ pages/user/@id/+Page.vue
@@ -5,11 +5,36 @@
 -->
 
 <script lang="ts" setup>
+import UpdateName from '@/components/user/UpdateName.vue'
+
 import type { Data } from './+data'
 import { useData } from 'vike-vue/useData'
 const data = useData<Data>()
 </script>
 
 <template>
-  user: {{ data.user.name }}
+  <section>
+    <header>
+      <h1>Edit User Profile</h1>
+    </header>
+    <main>
+      <UpdateName :name="data.user.name"/>
+    </main>
+    <footer>
+    </footer>
+  </section>
 </template>
+
+<style scoped>
+section {
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+}
+header {
+  text-align: center;
+}
+.right {
+  float: right;
+}
+</style>
