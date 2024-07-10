1a5dff4b08a0aa9d78e80ba09aff1c85d7042a13
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-10 09:55:53 -0400

    sync

--- components/admin/WorldImport.telefunc.ts
+++ components/admin/WorldImport.telefunc.ts
@@ -7,8 +7,9 @@
 /**
  * Scans for worlds not yet imported
  */
-export const onWorldImport = async ():Promise<void> => {
+export const onWorldImport = async ({ url }:{ url:Response }):Promise<void> => {
   console.log('Importing world...')
+  console.log(url)
   await new Promise(r => setTimeout(r, 2000))
   console.log('Import sim done!')
 }
--- components/admin/WorldImport.vue
+++ components/admin/WorldImport.vue
@@ -13,16 +13,20 @@ const filePicker = ref()
 const importWorld = async (event:any) => {
   if(window.confirm('Import world?')) {
     try {
-      await onWorldImport()
+      const form = event.currentTarget
+      console.log(form)
+      const url = new URL(form.action)
+      await onWorldImport({ url: await fetch(url) })
       window.alert('World imported!')
     } catch (error:any) {
       window.alert(error.message)
     }
   }
+  event.preventDefault()
 }
 </script>
 
 <template>
   <label for="worldimport">Import world:</label>
-  <input id="worldimport" type="file" accept="text/plain" :onchange="importWorld"/>
+  <input id="worldimport" type="file" accept=".zip" :onchange="importWorld"/>
 </template>
