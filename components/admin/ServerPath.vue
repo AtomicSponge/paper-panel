74657e540c8ac58a95b40367d585781902066413
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-09 23:20:08 -0400

    sync

--- components/admin/ServerPath.vue
+++ components/admin/ServerPath.vue
@@ -13,12 +13,24 @@ const props = defineProps<{
 }>()
 
 const serverPath = ref(props.path)
+
+const updatePath = async () => {
+  //
+}
 </script>
 
 <template>
   <label for="serverpath">Server path:</label>
   <input id="serverpath" type="text" v-model="serverPath"/>
+  <button @click="updatePath">Update</button>
 </template>
 
 <style scoped>
+label {
+  margin-right: 1em;
+}
+input {
+  margin-right: 3em;
+  width: 14em;
+}
 </style>
