05d9d15a42ec9566e835ad79479062acd430a0ce
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-09 22:54:13 -0400

    sync

--- components/user/UpdateName.telefunc.ts
+++ components/user/UpdateName.telefunc.ts
@@ -7,7 +7,6 @@
 import { JSONFilePreset } from 'lowdb/node'
 
 import { users } from '@/database/users'
-import type { Users } from '@/database/users'
 
 /**
  * Updates a user's display name
@@ -15,8 +14,6 @@ import type { Users } from '@/database/users'
  * @param name Name to update to
  */
 export const onNameUpdate = async ({ id, name }:{ id:number, name:string }):Promise<void> => {
-  console.log(`Updating user name ${name}...`)
-
   const db = await JSONFilePreset('db.json', users)
   await db.update(({ user }) => {
     const elem = user.find(user => user.id === id)
