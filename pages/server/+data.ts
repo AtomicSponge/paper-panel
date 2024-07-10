15d1903e4e5b89caa1eccdc5b95987db24873754
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-10 18:37:13 -0400

    sync

--- pages/server/+data.ts
+++ pages/server/+data.ts
@@ -6,6 +6,7 @@
 
 import fs from 'node:fs'
 import path from 'node:path'
+import YAML from 'yaml'
 import { JSONFilePreset } from 'lowdb/node'
 
 import { server } from '@/database/server'
@@ -27,23 +28,41 @@ export const data = async (pageContext:PageContextServer) => {
 
   if(data !== undefined && fs.existsSync(data.path)) {
     try {
-      //  Config files
+      //  Load config files
       paperConfig = fs.readFileSync(path.join(data.path, 'config', 'paper-global.yml'))
       paperWorldDefaults = fs.readFileSync(path.join(data.path, 'config', 'paper-world-defaults.yml'))
       bukkitConfig = fs.readFileSync(path.join(data.path, 'bukkit.yml'))
       spigotConfig = fs.readFileSync(path.join(data.path, 'spigot.yml'))
-      //  User files
+      //  Load user files
       operators = fs.readFileSync(path.join(data.path, 'ops.json'))
       bannedIps = fs.readFileSync(path.join(data.path, 'banned-ips.json'))
       bannedPlayers = fs.readFileSync(path.join(data.path, 'banned-players.json'))
       whitelist = fs.readFileSync(path.join(data.path, 'whitelist.json'))
+      //  Parse YAML
+      paperConfig = YAML.parse(paperConfig.toString())
+      paperWorldDefaults = YAML.parse(paperWorldDefaults.toString())
+      bukkitConfig = YAML.parse(bukkitConfig.toString())
+      spigotConfig = YAML.parse(spigotConfig.toString())
+      //  Parse JSON
+      operators = JSON.parse(operators.toString())
+      bannedIps = JSON.parse(bannedIps.toString())
+      bannedPlayers = JSON.parse(bannedPlayers.toString())
+      whitelist = JSON.parse(whitelist.toString())
     } catch (error:any) {
       throw error
     }
   }
 
   return {
-    server: data
+    server: data,
+    paperConfig,
+    paperWorldDefaults,
+    bukkitConfig,
+    spigotConfig,
+    operators,
+    bannedIps,
+    bannedPlayers,
+    whitelist
   }
 }
 
