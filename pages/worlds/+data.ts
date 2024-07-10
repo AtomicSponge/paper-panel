02ceb158560384a29e98b89e52587213e725d0f5
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-09 20:45:39 -0400

    sync

--- pages/worlds/+data.ts
+++ pages/worlds/+data.ts
@@ -9,7 +9,7 @@ import { worlds } from '@/database/worlds'
 import type { PageContextServer } from 'vike/types'
 
 export const data = async (pageContext:PageContextServer) => {
-  const isAdmin = Boolean(pageContext.user?.admin)
+  const isAdmin = Boolean(pageContext.user?.admin) || Boolean(pageContext.user?.serveradmin)
   const userId = Number(pageContext.user?.id)
 
   const data = isAdmin ? worlds.world : worlds.world.filter(world => world.users.includes(userId))
