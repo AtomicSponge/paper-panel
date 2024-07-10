02cab28b3967aed8ca058e4268727cf5c865277b
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-10 18:31:06 -0400

    sync

--- pages/server/+data.ts
+++ pages/server/+data.ts
@@ -27,11 +27,12 @@ export const data = async (pageContext:PageContextServer) => {
 
   if(data !== undefined && fs.existsSync(data.path)) {
     try {
+      //  Config files
       paperConfig = fs.readFileSync(path.join(data.path, 'config', 'paper-global.yml'))
       paperWorldDefaults = fs.readFileSync(path.join(data.path, 'config', 'paper-world-defaults.yml'))
-
       bukkitConfig = fs.readFileSync(path.join(data.path, 'bukkit.yml'))
       spigotConfig = fs.readFileSync(path.join(data.path, 'spigot.yml'))
+      //  User files
       operators = fs.readFileSync(path.join(data.path, 'ops.json'))
       bannedIps = fs.readFileSync(path.join(data.path, 'banned-ips.json'))
       bannedPlayers = fs.readFileSync(path.join(data.path, 'banned-players.json'))
