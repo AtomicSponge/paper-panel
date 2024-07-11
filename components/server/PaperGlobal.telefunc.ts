7c93bd2d07da88f6054e442db34dee6480894b69
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-11 09:30:02 -0400

    sync

--- components/server/PaperGlobal.telefunc.ts
+++ components/server/PaperGlobal.telefunc.ts
@@ -4,10 +4,26 @@
  * See LICENSE.md
  */
 
+import fs from 'node:fs'
+import path from 'node:path'
+import YAML from 'yaml'
+import { JSONFilePreset } from 'lowdb/node'
+
+import { server } from '@/database/server'
+
 /**
  * Save server configuration
  */
 export const onSave = async (data:any):Promise<void> => {
   console.log('Saving Paper Global config...')
   console.log(data)
+
+  const db = await JSONFilePreset('db.json', server)
+  const serverSettings = db.data.server.at(0)
+
+  if (serverSettings !== undefined && fs.existsSync(serverSettings.path)) {
+    if (data === undefined) throw new Error('Unable to save!  Missing data!')
+  } else {
+    throw new Error('Unable to find server path!')
+  }
 }
