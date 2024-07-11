7b56a14633bfa56cc4ed64c9d004a0de91d84fa1
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-11 09:37:47 -0400

    sync

--- components/server/Bukkit.telefunc.ts
+++ components/server/Bukkit.telefunc.ts
@@ -4,10 +4,27 @@
  * See LICENSE.md
  */
 
+import fs from 'node:fs'
+import path from 'node:path'
+import YAML from 'yaml'
+import { JSONFilePreset } from 'lowdb/node'
+
+import { server } from '@/database/server'
+
 /**
  * Save Bukkit configuration
  */
 export const onSave = async (data:any):Promise<void> => {
   console.log('Saving Bukkit config...')
   console.log(data)
+
+  const db = await JSONFilePreset('db.json', server)
+  const serverSettings = db.data.server.at(0)
+
+  if (serverSettings !== undefined && fs.existsSync(serverSettings.path)) {
+    if (data === undefined) throw new Error('Unable to save!  Missing data!')
+    fs.writeFileSync(path.join(serverSettings.path, 'bukkit.yml'), YAML.stringify(data))
+  } else {
+    throw new Error('Unable to find server path!')
+  }
 }
--- components/server/PaperWorldDefaults.telefunc.ts
+++ components/server/PaperWorldDefaults.telefunc.ts
@@ -4,10 +4,27 @@
  * See LICENSE.md
  */
 
+import fs from 'node:fs'
+import path from 'node:path'
+import YAML from 'yaml'
+import { JSONFilePreset } from 'lowdb/node'
+
+import { server } from '@/database/server'
+
 /**
- * Save World defaults
+ * Save World Defaults configuration
  */
 export const onSave = async (data:any):Promise<void> => {
-  console.log('Saving Paper world defaults...')
+  console.log('Saving World Defaults config...')
   console.log(data)
+
+  const db = await JSONFilePreset('db.json', server)
+  const serverSettings = db.data.server.at(0)
+
+  if (serverSettings !== undefined && fs.existsSync(serverSettings.path)) {
+    if (data === undefined) throw new Error('Unable to save!  Missing data!')
+    fs.writeFileSync(path.join(serverSettings.path, 'config', 'paper-world-defaults.yml'), YAML.stringify(data))
+  } else {
+    throw new Error('Unable to find server path!')
+  }
 }
