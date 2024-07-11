3f7a24e7e8e9204f4219f871da05d8f3dc73df99
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-11 11:11:46 -0400

    sync

--- components/setup/AdminConfig.vue
+++ components/setup/AdminConfig.vue
@@ -13,7 +13,18 @@
       <h2>Admin config</h2>
     </header>
     <main>
-      ...
+      <div>
+        <label for="username">Username:</label>
+        <input type="text" id="username">
+      </div>
+      <div>
+        <label for="password">Password:</label>
+        <input type="password" id="password">
+      </div>
+      <div>
+        <label for="cpassword">Confirm Password:</label>
+        <input type="password" id="cpassword">
+      </div>
     </main>
   </section>
 </template>
