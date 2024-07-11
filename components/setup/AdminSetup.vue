f3d5f32da99bdec5eb892fd79216b99a9dac8b82
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-11 16:24:18 -0400

    sync

--- components/setup/AdminSetup.vue
+++ components/setup/AdminSetup.vue
@@ -16,21 +16,39 @@ const data:ModelRef<AdminSetupData> = defineModel({ required: true })
       <h2>Admin config</h2>
     </header>
     <main>
-      <div>
-        <label for="username">Username:</label>
-        <input type="text" id="username" v-model="data.username">
-      </div>
-      <div>
-        <label for="username">Display Name:</label>
-        <input type="text" id="username" v-model="data.display">
-      </div>
-      <div>
-        <label for="password">Password:</label>
-        <input type="password" id="password" v-model="data.password">
-      </div>
-      <div>
-        <label for="cpassword">Confirm Password:</label>
-        <input type="password" id="cpassword" v-model="data.confirm">
+      <div class="table">
+        <div class="row">
+          <div class="cell">
+            <label for="username">Username:</label>
+          </div>
+          <div class="cell">
+            <input type="text" id="username" v-model="data.username">
+          </div>
+        </div>
+        <div class="row">
+          <div class="cell">
+            <label for="username">Display Name:</label>
+          </div>
+          <div class="cell">
+            <input type="text" id="username" v-model="data.display">
+          </div>
+        </div>
+        <div class="row">
+          <div class="cell">
+            <label for="password">Password:</label>
+          </div>
+          <div class="cell">
+            <input type="password" id="password" v-model="data.password">
+          </div>
+        </div>
+        <div class="row">
+          <div class="cell">
+            <label for="cpassword">Confirm Password:</label>
+          </div>
+          <div class="cell">
+            <input type="password" id="cpassword" v-model="data.confirm">
+          </div>
+        </div>
       </div>
     </main>
   </section>
@@ -52,5 +70,14 @@ main {
 }
 div {
   padding: 0.4em;
+}
+div .table {
+  display: table;
+}
+div .row {
+  display: table-row;
+}
+div .cell {
+  display: table-cell;
 }
 </style>
