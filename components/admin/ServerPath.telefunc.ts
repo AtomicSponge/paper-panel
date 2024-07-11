5100b6d5a1c154801d5069253d641e0b8d943075
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-11 15:37:27 -0400

    sync

--- components/admin/ServerPath.telefunc.ts
+++ components/admin/ServerPath.telefunc.ts
@@ -4,13 +4,17 @@
  * See LICENSE.md
  */
 
+import fs from 'node:fs'
 import { JSONFilePreset } from 'lowdb/node'
 
 import { server } from '@/database/server'
 
 export const onUpdateServerPath = async ({ path }:{ path:string }) => {
   const db = await JSONFilePreset('db.json', server)
+
   if(db.data.server === undefined) return { errorMessage: 'Server data not found!' }
+  if(!fs.existsSync(path)) return { errorMessage: `Unable to access path '${path}'!` }
+
   await db.update(({ server }) => {
     server[0].path = path
   })
