812458bc467bd7766823ec2b006ca3c56e0f609e
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-11 11:07:52 -0400

    sync

--- components/setup/AdminConfig.vue
+++ components/setup/AdminConfig.vue
@@ -8,4 +8,31 @@
 </script>
 
 <template>
+  <section class="subbox">
+    <header>
+      <h2>Admin config</h2>
+    </header>
+    <main>
+      ...
+    </main>
+  </section>
 </template>
+
+<style scoped>
+section {
+  display: flex;
+  flex-flow: column;
+  align-items: stretch;
+  margin: 1em;
+  padding: 1em;
+}
+header {
+  text-align: center;
+}
+main {
+  padding: 0.4em;
+}
+div {
+  padding: 0.4em;
+}
+</style>
