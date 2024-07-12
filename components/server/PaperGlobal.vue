a294dae19a542c4a75019789185193dbdab3fe4b
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-12 08:08:53 -0400

    sync

--- components/server/PaperGlobal.vue
+++ components/server/PaperGlobal.vue
@@ -47,19 +47,19 @@ const saveConfig = async ():Promise<void> => {
         <div>block-updates</div>
         <div class="tab">
           disable-chorus-plant-updates
-          <input type="checkbox" v-model="data['block-updates']['disable-chorus-plant-updates']">
+          <input type="checkbox" v-model="data['block-updates']['disable-chorus-plant-updates']"/>
         </div>
         <div class="tab">
           disable-mushroom-block-updates
-          <input type="checkbox" v-model="data['block-updates']['disable-mushroom-block-updates']">
+          <input type="checkbox" v-model="data['block-updates']['disable-mushroom-block-updates']"/>
         </div>
         <div class="tab">
           disable-noteblock-updates
-          <input type="checkbox" v-model="data['block-updates']['disable-noteblock-updates']">
+          <input type="checkbox" v-model="data['block-updates']['disable-noteblock-updates']"/>
         </div>
         <div class="tab">
           disable-tripwire-updates
-          <input type="checkbox" v-model="data['block-updates']['disable-tripwire-updates']">
+          <input type="checkbox" v-model="data['block-updates']['disable-tripwire-updates']"/>
         </div>
       </div>
 
@@ -67,15 +67,15 @@ const saveConfig = async ():Promise<void> => {
         <div>chunk-loading-advanced</div>
         <div class="tab">
           auto-config-send-distance
-          <input type="checkbox" v-model="data['chunk-loading-advanced']['auto-config-send-distance']">
+          <input type="checkbox" v-model="data['chunk-loading-advanced']['auto-config-send-distance']"/>
         </div>
         <div class="tab">
           player-max-concurrent-chunk-generates
-          <input type="number" v-model.number="data['chunk-loading-advanced']['player-max-concurrent-chunk-generates']">
+          <input type="number" v-model.number="data['chunk-loading-advanced']['player-max-concurrent-chunk-generates']"/>
         </div>
         <div class="tab">
           player-max-concurrent-chunk-loads
-          <input type="number" v-model.number="data['chunk-loading-advanced']['player-max-concurrent-chunk-loads']">
+          <input type="number" v-model.number="data['chunk-loading-advanced']['player-max-concurrent-chunk-loads']"/>
         </div>
       </div>
 
@@ -83,15 +83,15 @@ const saveConfig = async ():Promise<void> => {
         <div>chunk-loading-basic</div>
         <div class="tab">
           player-max-chunk-generate-rate
-          <input type="number" v-model.number="data['chunk-loading-basic']['player-max-chunk-generate-rate']">
+          <input type="number" v-model.number="data['chunk-loading-basic']['player-max-chunk-generate-rate']"/>
         </div>
         <div class="tab">
           player-max-chunk-load-rate
-          <input type="number" v-model.number="data['chunk-loading-basic']['player-max-chunk-load-rate']">
+          <input type="number" v-model.number="data['chunk-loading-basic']['player-max-chunk-load-rate']"/>
         </div>
         <div class="tab">
           player-max-chunk-send-rate
-          <input type="number" v-model.number="data['chunk-loading-basic']['player-max-chunk-send-rate']">
+          <input type="number" v-model.number="data['chunk-loading-basic']['player-max-chunk-send-rate']"/>
         </div>
       </div>
 
@@ -107,11 +107,11 @@ const saveConfig = async ():Promise<void> => {
         </div>
         <div class="tab">
           player-max-chunk-send-rate
-          <input type="number" v-model.number="data['chunk-system']['io-threads']">
+          <input type="number" v-model.number="data['chunk-system']['io-threads']"/>
         </div>
         <div class="tab">
           player-max-chunk-send-rate
-          <input type="number" v-model.number="data['chunk-system']['worker-threads']">
+          <input type="number" v-model.number="data['chunk-system']['worker-threads']"/>
         </div>
       </div>
 
@@ -119,11 +119,11 @@ const saveConfig = async ():Promise<void> => {
         <div>collisions</div>
         <div class="tab">
           enable-player-collisions
-          <input type="checkbox" v-model="data['collisions']['enable-player-collisions']">
+          <input type="checkbox" v-model="data['collisions']['enable-player-collisions']"/>
         </div>
         <div class="tab">
           send-full-pos-for-hard-colliding-entities
-          <input type="checkbox" v-model="data['collisions']['send-full-pos-for-hard-colliding-entities']">
+          <input type="checkbox" v-model="data['collisions']['send-full-pos-for-hard-colliding-entities']"/>
         </div>
       </div>
 
@@ -131,15 +131,15 @@ const saveConfig = async ():Promise<void> => {
         <div>commands</div>
         <div class="tab">
           fix-target-selector-tag-completion
-          <input type="checkbox" v-model="data['commands']['fix-target-selector-tag-completion']">
+          <input type="checkbox" v-model="data['commands']['fix-target-selector-tag-completion']"/>
         </div>
         <div class="tab">
           suggest-player-names-when-null-tab-completions
-          <input type="checkbox" v-model="data['commands']['suggest-player-names-when-null-tab-completions']">
+          <input type="checkbox" v-model="data['commands']['suggest-player-names-when-null-tab-completions']"/>
         </div>
         <div class="tab">
           time-command-affects-all-worlds
-          <input type="checkbox" v-model="data['commands']['time-command-affects-all-worlds']">
+          <input type="checkbox" v-model="data['commands']['time-command-affects-all-worlds']"/>
         </div>
       </div>
 
@@ -147,15 +147,15 @@ const saveConfig = async ():Promise<void> => {
         <div>console</div>
         <div class="tab">
           enable-brigadier-completions
-          <input type="checkbox" v-model="data['console']['enable-brigadier-completions']">
+          <input type="checkbox" v-model="data['console']['enable-brigadier-completions']"/>
         </div>
         <div class="tab">
           enable-brigadier-highlighting
-          <input type="checkbox" v-model="data['console']['enable-brigadier-highlighting']">
+          <input type="checkbox" v-model="data['console']['enable-brigadier-highlighting']"/>
         </div>
         <div class="tab">
           has-all-permissions
-          <input type="checkbox" v-model="data['console']['has-all-permissions']">
+          <input type="checkbox" v-model="data['console']['has-all-permissions']"/>
         </div>
       </div>
 
@@ -165,39 +165,39 @@ const saveConfig = async ():Promise<void> => {
           book
           <div class="tab">
             author
-            <input type="number" v-model.number="data['item-validation']['book']['author']">
+            <input type="number" v-model.number="data['item-validation']['book']['author']"/>
           </div>
           <div class="tab">
             page
-            <input type="number" v-model.number="data['item-validation']['book']['page']">
+            <input type="number" v-model.number="data['item-validation']['book']['page']"/>
           </div>
           <div class="tab">
             title
-            <input type="number" v-model.number="data['item-validation']['book']['title']">
+            <input type="number" v-model.number="data['item-validation']['book']['title']"/>
           </div>
         </div>
         <div class="tab">
           book-size
           <div class="tab">
             page-max
-            <input type="number" v-model.number="data['item-validation']['book-size']['page-max']">
+            <input type="number" v-model.number="data['item-validation']['book-size']['page-max']"/>
           </div>
           <div class="tab">
             total-multiplier
-            <input type="number" v-model.number="data['item-validation']['book-size']['total-multiplier']">
+            <input type="number" v-model.number="data['item-validation']['book-size']['total-multiplier']"/>
           </div>
         </div>
         <div class="tab">
           display-name
-            <input type="number" v-model.number="data['item-validation']['display-name']">
+            <input type="number" v-model.number="data['item-validation']['display-name']"/>
         </div>
         <div class="tab">
           lore-line
-            <input type="number" v-model.number="data['item-validation']['lore-line']">
+            <input type="number" v-model.number="data['item-validation']['lore-line']"/>
         </div>
         <div class="tab">
           resolve-selectors-in-books
-          <input type="checkbox" v-model="data['item-validation']['resolve-selectors-in-books']">
+          <input type="checkbox" v-model="data['item-validation']['resolve-selectors-in-books']"/>
         </div>
       </div>
 
@@ -205,7 +205,7 @@ const saveConfig = async ():Promise<void> => {
         <div>logging</div>
         <div class="tab">
           deobfuscate-stacktraces
-          <input type="checkbox" v-model="data['logging']['deobfuscate-stacktraces']">
+          <input type="checkbox" v-model="data['logging']['deobfuscate-stacktraces']"/>
         </div>
       </div>
 
@@ -236,7 +236,7 @@ const saveConfig = async ():Promise<void> => {
           </div>
           <div>
             use-display-name-in-quit-message
-            <input type="checkbox" v-model="data['messages']['use-display-name-in-quit-message']">
+            <input type="checkbox" v-model="data['messages']['use-display-name-in-quit-message']"/>
           </div>
         </div>
       </div>
@@ -247,44 +247,44 @@ const saveConfig = async ():Promise<void> => {
           chat-threads
           <div class="tab">
             chat-executor-core-size
-            <input type="number" v-model.number="data['misc']['chat-threads']['chat-executor-core-size']">
+            <input type="number" v-model.number="data['misc']['chat-threads']['chat-executor-core-size']"/>
           </div>
           <div class="tab">
             chat-executor-max-size
-            <input type="number" v-model.number="data['misc']['chat-threads']['chat-executor-max-size']">
+            <input type="number" v-model.number="data['misc']['chat-threads']['chat-executor-max-size']"/>
           </div>
         </div>
         <div class="tab">
           compression-level
-          <input type="text" v-model="data['misc']['compression-level']">
+          <input type="text" v-model="data['misc']['compression-level']"/>
         </div>
         <div class="tab">
           fix-entity-position-desync
-          <input type="checkbox" v-model="data['misc']['fix-entity-position-desync']">
+          <input type="checkbox" v-model="data['misc']['fix-entity-position-desync']"/>
         </div>
         <div class="tab">
           load-permissions-yml-before-plugins
-          <input type="checkbox" v-model="data['misc']['load-permissions-yml-before-plugins']">
+          <input type="checkbox" v-model="data['misc']['load-permissions-yml-before-plugins']"/>
         </div>
         <div class="tab">
           max-joins-per-tick
-          <input type="number" v-model.number="data['misc']['max-joins-per-tick']">
+          <input type="number" v-model.number="data['misc']['max-joins-per-tick']"/>
         </div>
         <div class="tab">
           region-file-cache-size
-          <input type="number" v-model.number="data['misc']['region-file-cache-size']">
+          <input type="number" v-model.number="data['misc']['region-file-cache-size']"/>
         </div>
         <div class="tab">
           strict-advancement-dimension-check
-          <input type="checkbox" v-model="data['misc']['strict-advancement-dimension-check']">
+          <input type="checkbox" v-model="data['misc']['strict-advancement-dimension-check']"/>
         </div>
         <div class="tab">
           use-alternative-luck-formula
-          <input type="checkbox" v-model="data['misc']['use-alternative-luck-formula']">
+          <input type="checkbox" v-model="data['misc']['use-alternative-luck-formula']"/>
         </div>
         <div class="tab">
           use-dimension-type-for-custom-spawners
-          <input type="checkbox" v-model="data['misc']['use-dimension-type-for-custom-spawners']">
+          <input type="checkbox" v-model="data['misc']['use-dimension-type-for-custom-spawners']"/>
         </div>
       </div>
 
@@ -316,23 +316,23 @@ const saveConfig = async ():Promise<void> => {
         <h3>Unsupported Settings</h3>
         <div class="tab">
           allow-headless-pistons
-          <input type="checkbox" v-model="data['unsupported-settings']['allow-headless-pistons']">
+          <input type="checkbox" v-model="data['unsupported-settings']['allow-headless-pistons']"/>
         </div>
         <div class="tab">
           allow-permanent-block-break-exploits
-          <input type="checkbox" v-model="data['unsupported-settings']['allow-permanent-block-break-exploits']">
+          <input type="checkbox" v-model="data['unsupported-settings']['allow-permanent-block-break-exploits']"/>
         </div>
         <div class="tab">
           allow-piston-duplication
-          <input type="checkbox" v-model="data['unsupported-settings']['allow-piston-duplication']">
+          <input type="checkbox" v-model="data['unsupported-settings']['allow-piston-duplication']"/>
         </div>
         <div class="tab">
           allow-tripwire-disarming-exploits
-          <input type="checkbox" v-model="data['unsupported-settings']['allow-tripwire-disarming-exploits']">
+          <input type="checkbox" v-model="data['unsupported-settings']['allow-tripwire-disarming-exploits']"/>
         </div>
         <div class="tab">
           allow-unsafe-end-portal-teleportation
-          <input type="checkbox" v-model="data['unsupported-settings']['allow-unsafe-end-portal-teleportation']">
+          <input type="checkbox" v-model="data['unsupported-settings']['allow-unsafe-end-portal-teleportation']"/>
         </div>
         <div class="tab">
           compression-format
@@ -344,11 +344,11 @@ const saveConfig = async ():Promise<void> => {
         </div>
         <div class="tab">
           perform-username-validation
-          <input type="checkbox" v-model="data['unsupported-settings']['perform-username-validation']">
+          <input type="checkbox" v-model="data['unsupported-settings']['perform-username-validation']"/>
         </div>
         <div class="tab">
           skip-vanilla-damage-tick-when-shield-blocked
-          <input type="checkbox" v-model="data['unsupported-settings']['skip-vanilla-damage-tick-when-shield-blocked']">
+          <input type="checkbox" v-model="data['unsupported-settings']['skip-vanilla-damage-tick-when-shield-blocked']"/>
         </div>
       </div>
 
@@ -356,11 +356,11 @@ const saveConfig = async ():Promise<void> => {
         <div>watchdog</div>
         <div class="tab">
           early-warning-delay
-          <input type="number" v-model.number="data['watchdog']['early-warning-delay']">
+          <input type="number" v-model.number="data['watchdog']['early-warning-delay']"/>
         </div>
         <div class="tab">
           early-warning-every
-          <input type="number" v-model.number="data['watchdog']['early-warning-every']">
+          <input type="number" v-model.number="data['watchdog']['early-warning-every']"/>
         </div>
       </div>
     </main>
