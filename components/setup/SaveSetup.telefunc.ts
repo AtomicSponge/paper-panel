0a14c8cf69a7a467af3da634c83e8b96112ddfaa
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-12 09:21:46 -0400

    sync

--- components/setup/SaveSetup.telefunc.ts
+++ components/setup/SaveSetup.telefunc.ts
@@ -19,20 +19,36 @@ export const onSave = async ({ adminData, serverConfig }:{ adminData:AdminSetupD
   console.log(adminData)
   console.log(serverConfig)
 
+  /** Admin configuration */
   if(adminData.password !== adminData.confirm) {
     return { errorMessage: 'Passwords do not match!' }
   }
 
+  const salt = ''
+  const password = ''
+
+  /** Server configuration */
   if(!fs.existsSync(serverConfig.path)) {
     return { errorMessage: 'Unable to access path!' }
   }
 
   return { errorMessage: 'Early stop' }
 
+  /** Create initial database */
   const serverDb = await JSONFilePreset('db.json', server)
   await serverDb.update(({ server }) => { server.push({ path: serverConfig.path }) })
   const usersDb = await JSONFilePreset('db.json', users)
-  //await usersDb.update(() => {})
+  await usersDb.update(({ user }) => { user.push(
+    {
+      id: 0,
+      name: adminData.display,
+      login: adminData.username,
+      password: password,
+      salt: salt,
+      admin: true,
+      serveradmin: true
+    }
+  ) })
   const worldsDb = await JSONFilePreset('db.json', worlds)
   //await worldsDb.update(() => {})
 }
