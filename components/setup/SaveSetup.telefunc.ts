e4789fee031d148d51cef5cd4133aaa0eb68d232
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-11 21:06:55 -0400

    sync

--- components/setup/SaveSetup.telefunc.ts
+++ components/setup/SaveSetup.telefunc.ts
@@ -4,6 +4,12 @@
  * See LICENSE.md
  */
 
+import { JSONFilePreset } from 'lowdb/node'
+
+import { server } from '@/database/server'
+import { users } from '@/database/users'
+import { worlds } from '@/database/worlds'
+
 /**
  * Save first time configuration
  */
@@ -15,4 +21,11 @@ export const onSave = async ({ adminData, serverConfig }:{ adminData:AdminSetupD
   if(adminData.password !== adminData.confirm) {
     return { errorMessage: 'Admin passwords do not match!' }
   }
+
+  const serverDb = await JSONFilePreset('db.json', server)
+  //await serverDb.update(() => {})
+  const usersDb = await JSONFilePreset('db.json', users)
+  //await usersDb.update(() => {})
+  const worldsDb = await JSONFilePreset('db.json', worlds)
+  //await worldsDb.update(() => {})
 }
