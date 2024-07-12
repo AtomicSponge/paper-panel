b285ba3adeb799de707f0a763b26d8db518050eb
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-12 09:12:29 -0400

    sync

--- components/setup/SaveSetup.telefunc.ts
+++ components/setup/SaveSetup.telefunc.ts
@@ -15,22 +15,6 @@ import { worlds } from '@/database/worlds'
  * Save first time configuration
  */
 export const onSave = async ({ adminData, serverConfig }:{ adminData:AdminSetupData, serverConfig:ServerSetupData }) => {
-  /**
-   * Check access permissions
-   * @param file File or folder to check
-   * @param mask Permissions (decimal value) to check
-   * @param cb Callback function
-   */
-  const checkPermissions = (file:string, mask:number, cb:Function) => {
-    fs.stat(file, (error, stats) => {
-      if (error) {
-        cb(error, false)
-      } else {
-        cb(null, !!(mask & parseInt ((stats.mode & parseInt ("777", 8)).toString (8)[0])))
-      }
-    })
-  }
-
   console.log('saving initial config')
   console.log(adminData)
   console.log(serverConfig)
@@ -42,12 +26,6 @@ export const onSave = async ({ adminData, serverConfig }:{ adminData:AdminSetupD
   if(!fs.existsSync(serverConfig.path)) {
     return { errorMessage: 'Unable to access path!' }
   }
-  checkPermissions(serverConfig.path, 4, (error:any) => {
-    if (error) return { errorMessage: 'Unable to read path!' }
-  })
-  checkPermissions(serverConfig.path, 2, (error:any) => {
-    if (error) return { errorMessage: 'Unable to write path!' }
-  })
 
   return { errorMessage: 'Early stop' }
 
