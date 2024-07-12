bca1da83027f20bb2d2ce5f842ed0c81d314be28
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-11 23:04:10 -0400

    sync

---
+++ components/server/ServerProperties.telefunc.ts
@@ -0,0 +1,16 @@
+/**
+ * paper-panel
+ * By:  Matthew Evans
+ * See LICENSE.md
+ */
+
+import fs from 'node:fs'
+import path from 'node:path'
+
+export const onSave = async (data:any) => {
+  try {
+    //
+  } catch (error:any) {
+    return { errorMessage: error.message }
+  }
+}
