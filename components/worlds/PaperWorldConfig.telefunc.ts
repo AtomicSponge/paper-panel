2e7386a78ce6f1db1157083285d65ea712cac1be
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-17 12:28:22 -0400

    sync

--- components/worlds/PaperWorldConfig.telefunc.ts
+++ components/worlds/PaperWorldConfig.telefunc.ts
@@ -4,12 +4,31 @@
  * See LICENSE.md
  */
 
+import fs from 'node:fs'
+import path from 'node:path'
+import { JSONFilePreset } from 'lowdb/node'
+
+import { server } from '@/database/server'
+
 /**
  * Save world configuration
  */
 export const onSave = async ({ folder, data }:{ folder:string, data:string }) => {
   console.log('Saving world config...')
-  if(false) {
-    return { errorMessage: '' }
+  console.log(data)
+
+  const db = await JSONFilePreset('db.json', server)
+  const serverSettings = db.data.server.at(0)
+
+  try {
+    if (serverSettings !== undefined && fs.existsSync(path.join(serverSettings.path, folder))) {
+      if (data === undefined) throw new Error('Unable to save!  Missing data!')
+      fs.writeFileSync(path.join(serverSettings.path, folder, 'paper-world.yml'), data)
+    } else {
+      throw new Error('Unable to find world path!')
+    }
+  } catch (error:any) {
+    console.error(error.message)
+    return { errorMessage: error.message }
   }
 }
