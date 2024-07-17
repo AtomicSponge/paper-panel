c88e52bd754b0c49d5ead1b3b362a2f500594824
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-17 18:50:03 -0400

    sync

--- components/server/Bukkit.vue
+++ components/server/Bukkit.vue
@@ -42,7 +42,137 @@ const saveConfig = async ():Promise<void> => {
       <div><button @click="toggleConfig">{{ label }}</button></div>
     </header>
     <main v-show="showConfig">
-      ...
+      <div>
+        <div>settings</div>
+        <div class="tab">
+          allow-end
+          <input type="checkbox" v-model="data['settings']['allow-end']"/>
+        </div>
+        <div class="tab">
+          warn-on-overload
+          <input type="checkbox" v-model="data['settings']['warn-on-overload']"/>
+        </div>
+        <div class="tab">
+          permissions-file
+          <input type="text" v-model="data['settings']['permissions-file']"/>
+        </div>
+        <div class="tab">
+          update-folder
+          <input type="text" v-model="data['settings']['update-folder']"/>
+        </div>
+        <div class="tab">
+          plugin-profiling
+          <input type="checkbox" v-model="data['settings']['plugin-profiling']"/>
+        </div>
+        <div class="tab">
+          connection-throttle
+          <input type="number" v-model="data['settings']['connection-throttle']"/>
+        </div>
+        <div class="tab">
+          query-plugins
+          <input type="checkbox" v-model="data['settings']['query-plugins']"/>
+        </div>
+        <div class="tab">
+          deprecated-verbose
+          <input type="text" v-model="data['settings']['deprecated-verbose']"/>
+        </div>
+        <div class="tab">
+          shutdown-message
+          <input type="text" v-model="data['settings']['shutdown-message']"/>
+        </div>
+        <div class="tab">
+          minimum-api
+          <input type="text" v-model="data['settings']['minimum-api']"/>
+        </div>
+        <div class="tab">
+          use-map-color-cache
+          <input type="checkbox" v-model="data['settings']['use-map-color-cache']"/>
+        </div>
+      </div>
+
+      <div>
+        <div>spawn-limits</div>
+        <div class="tab">
+          monsters
+          <input type="number" v-model="data['spawn-limits']['monsters']"/>
+        </div>
+        <div class="tab">
+          animals
+          <input type="number" v-model="data['spawn-limits']['animals']"/>
+        </div>
+        <div class="tab">
+          water-animals
+          <input type="number" v-model="data['spawn-limits']['water-animals']"/>
+        </div>
+        <div class="tab">
+          water-ambient
+          <input type="number" v-model="data['spawn-limits']['water-ambient']"/>
+        </div>
+        <div class="tab">
+          water-underground-creature
+          <input type="number" v-model="data['spawn-limits']['water-underground-creature']"/>
+        </div>
+        <div class="tab">
+          axolotls
+          <input type="number" v-model="data['spawn-limits']['axolotls']"/>
+        </div>
+        <div class="tab">
+          ambient
+          <input type="number" v-model="data['spawn-limits']['ambient']"/>
+        </div>
+      </div>
+
+      <div>
+        <div>chunk-gc</div>
+        <div class="tab">
+          period-in-ticks
+          <input type="number" v-model="data['chunk-gc']['period-in-ticks']"/>
+        </div>
+      </div>
+
+      <div>
+        <div>ticks-per</div>
+        <div class="tab">
+          animal-spawns
+          <input type="number" v-model="data['ticks-per']['animal-spawns']"/>
+        </div>
+
+        <div class="tab">
+          monster-spawns
+          <input type="number" v-model="data['ticks-per']['monster-spawns']"/>
+        </div>
+        <div class="tab">
+          water-spawns
+          <input type="number" v-model="data['ticks-per']['water-spawns']"/>
+        </div>
+        <div class="tab">
+          water-ambient-spawns
+          <input type="number" v-model="data['ticks-per']['water-ambient-spawns']"/>
+        </div>
+        <div class="tab">
+          water-underground-creature-spawns
+          <input type="number" v-model="data['ticks-per']['water-underground-creature-spawns']"/>
+        </div>
+        <div class="tab">
+          axolotl-spawns
+          <input type="number" v-model="data['ticks-per']['axolotl-spawns']"/>
+        </div>
+        <div class="tab">
+          ambient-spawns
+          <input type="number" v-model="data['ticks-per']['ambient-spawns']"/>
+        </div>
+        <div class="tab">
+          autosave
+          <input type="number" v-model="data['ticks-per']['autosave']"/>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          aliases
+          <input type="text" v-model="data['aliases']"/>
+        </div>
+      </div>
     </main>
     <footer v-show="showConfig">
       <button @click="saveConfig">Save Configuration</button>
