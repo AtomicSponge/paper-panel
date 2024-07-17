720f67fbf363f52eb9cd445dbb77c80c231c111b
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-17 14:55:33 -0400

    sync

--- components/admin/ResetSettings.telefunc.ts
+++ components/admin/ResetSettings.telefunc.ts
@@ -4,12 +4,25 @@
  * See LICENSE.md
  */
 
+import fs from 'node:fs'
+import path from 'node:path'
+import { JSONFilePreset } from 'lowdb/node'
+
+import { server } from '@/database/server'
+
 /**
  * Resets paper-panel settings
  */
 export const onResetSettings = async () => {
-  console.log('Reset settings...')
-  if(false) {
-    return { errorMessage: '' }
+  const db = await JSONFilePreset('db.json', server)
+  const serverSettings = db.data.server.at(0)
+
+  if(serverSettings === undefined)
+    return { errorMessage: 'Server path not found!' }
+  try {
+    fs.unlinkSync(path.join(serverSettings.path, 'db.json'))
+  } catch (error:any) {
+    console.error(error.message)
+    return { errorMessage: error.message }
   }
 }
