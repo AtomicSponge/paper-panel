bb344ca0e6909f4858a21a17e00abdd12de3f88c
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-21 14:12:00 -0400

    sync

--- layouts/HeadDefault.vue
+++ layouts/HeadDefault.vue
@@ -12,6 +12,6 @@
 <template>
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <meta name="description" content="Paper Panel" />
-  <meta http-equiv="Content-Security-Policy" content="script-src 'self' 'unsafe-inline'; style-src 'unsafe-inline';" />
+  <meta http-equiv="Content-Security-Policy" content="default-src 'none'; connect-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self'" />
   <link rel="icon" :href="logoUrl" />
 </template>
