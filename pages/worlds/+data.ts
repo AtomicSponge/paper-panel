ece4c522ae7b8e6f9199e3c05090834ba346b54f
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-22 11:10:39 -0400

    sync

--- pages/worlds/+data.ts
+++ pages/worlds/+data.ts
@@ -4,12 +4,15 @@
  * See LICENSE.md
  */
 
-import fs from 'node:fs'
+//import fs from 'node:fs'
+import fs from 'node:fs/promises'
 import path from 'node:path'
+import { existsSync } from 'node:fs'
 import { JSONFilePreset } from 'lowdb/node'
+import { asyncForEach } from '@spongex/async-for-each'
 
 import { server } from '@/database/server'
-import { worlds } from '@/database/worlds'
+import { World, worlds } from '@/database/worlds'
 
 import type { PageContextServer } from 'vike/types'
 
@@ -27,41 +30,41 @@ export const data = async (pageContext:PageContextServer) => {
   let netherConfig:Array<string> = []
   let theEndConfig:Array<string> = []
 
-  if(serverPath !== undefined && fs.existsSync(serverPath)) {
-    data.forEach(world => {
+  if(serverPath !== undefined && existsSync(serverPath)) {
+    await asyncForEach(data, async (world:World) => {
       const worldLocation = path.join(serverPath, world.folder, 'paper-world.yml')
       const netherLocation = path.join(serverPath, world.folder + '_nether', 'paper-world.yml')
       const theEndLocation = path.join(serverPath, world.folder + '_the_end', 'paper-world.yml')
 
       //  Read each file in or create if it does not exist
       try {
-        if(fs.existsSync(worldLocation)) {
-          worldConfig.push(fs.readFileSync(worldLocation).toString())
+        if(existsSync(worldLocation)) {
+          worldConfig.push((await fs.readFile(worldLocation)).toString())
         } else {
-          fs.closeSync(fs.openSync(worldLocation, 'r+'))
-          worldConfig.push(fs.readFileSync(worldLocation).toString())
+          await (await fs.open(worldLocation, 'r+')).close()
+          worldConfig.push((await fs.readFile(worldLocation)).toString())
         }
       } catch (error:any) {
         worldConfig.push('Problems editing file')
       }
 
       try {
-        if(fs.existsSync(netherLocation)) {
-          netherConfig.push(fs.readFileSync(netherLocation).toString())
+        if(existsSync(netherLocation)) {
+          netherConfig.push((await fs.readFile(netherLocation)).toString())
         } else {
-          fs.closeSync(fs.openSync(netherLocation, 'r+'))
-          netherConfig.push(fs.readFileSync(netherLocation).toString())
+          await (await fs.open(netherLocation, 'r+')).close()
+          netherConfig.push((await fs.readFile(netherLocation)).toString())
         }
       } catch (error:any) {
         netherConfig.push('Problems editing file')
       }
 
       try {
-        if(fs.existsSync(theEndLocation)) {
-          theEndConfig.push(fs.readFileSync(theEndLocation).toString())
+        if(existsSync(theEndLocation)) {
+          theEndConfig.push((await fs.readFile(theEndLocation)).toString())
         } else {
-          fs.closeSync(fs.openSync(theEndLocation, 'r+'))
-          theEndConfig.push(fs.readFileSync(theEndLocation).toString())
+          await (await fs.open(theEndLocation, 'r+')).close()
+          theEndConfig.push((await fs.readFile(theEndLocation)).toString())
         }
       } catch (error:any) {
         theEndConfig.push('Problems editing file')
