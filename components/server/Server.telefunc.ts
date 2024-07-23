ae25f5940f16a83a3ca1b2805a3a71786b4dca97
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-23 12:05:31 -0400

    sync

--- components/server/Server.telefunc.ts
+++ components/server/Server.telefunc.ts
@@ -4,6 +4,16 @@
  * See LICENSE.md
  */
 
+import path from 'node:path'
+import util from 'node:util'
+import { existsSync } from 'node:fs'
+import { exec as execAsync } from 'node:child_process'
+import { JSONFilePreset } from 'lowdb/node'
+
+import { server } from '@/database/server'
+
+const exec = util.promisify(execAsync)
+
 /**
  * Run an update on paper server
  */
@@ -12,9 +22,26 @@ export const onCheckUpdate = async () => {
   await new Promise(r => setTimeout(r, 2000))
   console.log('update check simulation wait done!')
 
-  if(false) {
-    return { errorMessage: 'Update available' }
+  const db = await JSONFilePreset('db.json', server)
+  const data = db.data.server.at(0)
+
+  if(data === undefined || data === null) {
+    return { errorMessage: 'Missing database settings!' }
   }
+
+  const currentVersion = await (async () => {
+    try {
+      const { stdout } = await exec(`java -jar ${data.filename} --version`, { cwd: data.path, windowsHide: true })
+      return stdout.toString().trim()
+    } catch (error:any) {
+      return null
+    }
+  })()
+  if(currentVersion === null) {
+    return { errorMessage: 'Unable to determine current version!' }
+  }
+
+  console.log(currentVersion)
 
   return { status: true }
 }
