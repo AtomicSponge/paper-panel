6ed702d1806f56a1e876f55da63dd3256c464997
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-24 10:39:17 -0400

    sync

--- components/worlds/PaperNetherConfig.telefunc.ts
+++ components/worlds/PaperNetherConfig.telefunc.ts
@@ -8,6 +8,7 @@ import fs from 'node:fs/promises'
 import path from 'node:path'
 import { existsSync } from 'node:fs'
 import { JSONFilePreset } from 'lowdb/node'
+import { __locale } from '@spongex/system-locale'
 
 import { server } from '@/database/server'
 
@@ -24,6 +25,8 @@ export const onSave = async ({ folder, data }:{ folder:string, data:string }) =>
     if (serverSettings !== undefined && existsSync(path.join(serverSettings.path, folder))) {
       if (data === undefined) throw new Error('Unable to save!  Missing data!')
       await fs.writeFile(path.join(serverSettings.path, folder, 'paper-world.yml'), data)
+      const updateDate = new Date().toLocaleString(__locale, { timeZoneName: 'short' })
+      console.log(`World Settings for ${folder} updated on ${updateDate}`)
     } else {
       throw new Error('Unable to save!  Cannot find world path!')
     }
