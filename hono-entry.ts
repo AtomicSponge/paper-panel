0680e43444b078c57794bd550ca154da9cba09c4
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-24 13:38:16 -0400

    sync

--- hono-entry.ts
+++ hono-entry.ts
@@ -1,8 +1,3 @@
-import {
-  authjsHandler,
-  authjsSessionMiddleware,
-} from './server/authjs-handler'
-
 import { vikeHandler } from './server/vike-handler'
 import { telefuncHandler } from './server/telefunc-handler'
 import { Hono } from 'hono'
@@ -40,13 +35,13 @@ export function handlerAdapter<
 
 const app = new Hono()
 
-app.use(handlerAdapter(authjsSessionMiddleware))
+//app.use(handlerAdapter(authjsSessionMiddleware))
 
 /**
  * Auth.js route
  * @link {@see https://authjs.dev/getting-started/installation}
  **/
-app.use('/api/auth/**', handlerAdapter(authjsHandler))
+//app.use('/api/auth/**', handlerAdapter(authjsHandler))
 
 app.post('/_telefunc', handlerAdapter(telefuncHandler))
 
