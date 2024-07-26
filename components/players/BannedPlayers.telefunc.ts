b15976a3835f033c9665a70660ad98a3dfbd6a3a
Matthew Evans <mevans+git@wtfsystems.net>
2024-07-26 09:50:38 -0400

    sync

--- components/players/BannedPlayers.telefunc.ts
+++ components/players/BannedPlayers.telefunc.ts
@@ -10,12 +10,15 @@ import { existsSync } from 'node:fs'
 import { JSONFilePreset } from 'lowdb/node'
 import { __locale } from '@spongex/system-locale'
 
+import { getUser } from '@/auth/getUser'
 import { server } from '@/database/server'
 
 /**
  * Update the Banned Players
  */
 export const onUpdate = async ({ data }:{ data:Array<string> }) => {
+  const user = getUser()
+
   const db = await JSONFilePreset('db.json', server)
   const serverSettings = db.data.server.at(0)
 
